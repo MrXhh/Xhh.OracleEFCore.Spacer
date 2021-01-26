ef457fe6963af24bddcd976ccf8263a2624d6de8
MrXhh <2791341417@qq.com>
2021-01-27 00:40:35 +0800

    feat:添加补丁功能

---
+++ src/OracleEFCore.Spacer/OracleEFCoreSpacers.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Query.Sql;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore.Update;
+using Oracle.EntityFrameworkCore.Migrations;
+using Oracle.EntityFrameworkCore.Query.Sql.Internal;
+using Oracle.EntityFrameworkCore.Storage.Internal;
+using Oracle.EntityFrameworkCore.Update.Internal;
+
+namespace Oracle.EntityFrameworkCore
+{
+    public static class OracleEFCoreSpacers
+    {
+        public static DbContextOptionsBuilder UseOracleEFCoreSpacers(this DbContextOptionsBuilder options)
+        {
+            options.ReplaceService<IUpdateSqlGenerator, OracleUpdateSqlGeneratorSpacer>();
+            options.ReplaceService<IMigrationsSqlGenerator, OracleMigrationsSqlGeneratorSpacer>();
+            options.ReplaceService<IRelationalDatabaseCreator, OracleDatabaseCreatorSpacer>();
+            options.ReplaceService<IQuerySqlGeneratorFactory, OracleQuerySqlGeneratorFactorySpacer>();
+
+
+            return options;
+        }
+
+    }
+}
